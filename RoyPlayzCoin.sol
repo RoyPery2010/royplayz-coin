dd8582520e3ad82bd4d0c09b40f198305be7a5be
RoyPery2010 <73431571+RoyPery2010@users.noreply.github.com>
2024-05-28 12:44:08 +0300

    Update RoyPlayzCoin.sol

--- RoyPlayzCoin.sol
+++ RoyPlayzCoin.sol
@@ -6,6 +6,12 @@ contract RoyPlayzCoin {
 	string public standard = "RoyPlayzCoin v1.0";
 	uint256 public totalSupply;
 
+	event Transfer(
+		address indexed _from,
+		address indexed _to,
+		uint256 _value
+	);
+
 	 mapping(address => uint256) public balanceOf;
 
 	constructor(uint256 _initialSupply) public {
@@ -14,5 +20,9 @@ contract RoyPlayzCoin {
 	}
 	function transfer(address _to, uint256 _value) public returns (bool success) {
 		require(balanceOf[msg.sender] >= _value);
+		balanceOf[msg.sender] -= _value;
+		balanceOf[_to] += _value;
+		emit Transfer(msg.sender, _to, _value);
+		return true;
 	}
 }
